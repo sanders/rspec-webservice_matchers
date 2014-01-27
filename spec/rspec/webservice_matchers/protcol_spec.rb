91d44f1cc1b3dbb6576af37015b92c8df18299aa
Robb Shecter <robb@weblaws.org>
2014-01-27 11:10:14 -0800

    First pass at test scenarios for json schema validation

--- spec/rspec/webservice_matchers/protcol_spec.rb
+++ spec/rspec/webservice_matchers/protcol_spec.rb
@@ -17,7 +17,6 @@ describe 'status_code' do
   end
 end
 
-
 describe 'be_up' do
   it 'follows redirects when necessary' do
     'weblaws.org'.should be_up
@@ -26,4 +25,12 @@ describe 'be_up' do
   it 'can also handle a simple 200' do
     'http://www.rfc-editor.org/rfc/rfc2616.txt'.should be_up
   end
+end
+
+describe 'json schema validation' do
+  it 'recognizes correct content'
+  it 'throws a meaningful exception if schema data not readable'
+  it 'throws a meaningful exception if the schema is not valid'
+  it 'throws a meaningful exception if the resource cannot be read'
+  it 'throws a meaningful exception if resource doesn\'t validate against the schema'
 end
